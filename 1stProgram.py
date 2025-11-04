1656dbfa78af120b09a32704472023f046c74352
MuhammadShahzaib-shah <102shahzaib@gmail.com>
2025-11-04 21:48:31 +0500

    first commit

---
+++ 1stProgram.py
@@ -0,0 +1,21 @@
+#name = "Muhammad Shahzaib Shah"
+#age = 20;
+#print(f"My name is {name} I am {age} years old.")
+
+#Type Conversion
+"""old_age = input("Enter your age :")
+new_age = int(old_age) + 2
+print(new_age)"""
+
+#String
+name = "Tony Stark"
+print(name.upper())
+print(name.lower())
+print(name.find('S'))
+print(name.replace("Stark","Shahzaib"))
+print(name.find('q'))
+print(name)
+print('n' in name)
+print('z' in name)
+
+
