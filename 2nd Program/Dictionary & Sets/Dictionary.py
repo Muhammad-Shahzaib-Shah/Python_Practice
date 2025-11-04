f3fc386423f930bb9498cdbd60fe117783175f03
MuhammadShahzaib-shah <102shahzaib@gmail.com>
2025-11-04 23:48:21 +0500

    Practice another day

---
+++ 2nd Program/Dictionary & Sets/Dictionary.py
@@ -0,0 +1,17 @@
+# Dictionary
+Marks = {
+    "Shahzaib" : 100,
+    "Ali" : 90, 
+    "Salam" :98
+
+}
+
+# Dictionary Methods
+
+print(Marks.items())
+print(Marks.keys())
+print(Marks.values())
+Marks.update({"Salam" : 12})
+print(Marks)
+# print(Marks.get("Sha1hzaib")) # It return an error
+# print(Marks["Shahzaib1"]) # It throw error
---
+++ 2nd Program/Dictionary & Sets/Sets.py
@@ -0,0 +1,12 @@
+# Sets
+a = set() # creating empting set
+a.add(1)
+a.add(13)
+a.add(33)
+a.add(2)
+a.add(2)
+a.add(11)
+print(a)
+a.remove(11)
+print(a)
+
