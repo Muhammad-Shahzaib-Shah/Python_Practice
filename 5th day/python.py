f1846196043094a197cfa844313e0203b2850be3
MuhammadShahzaib-shah <102shahzaib@gmail.com>
2025-11-09 14:17:43 +0500

    Another Day of coding

---
+++ 5th day/python.py
@@ -0,0 +1,10 @@
+#Function Definition
+def avg():
+    a = int(input("Enter 1st number : "))
+    b = int(input("Enter 2nd number : "))
+    c = int(input("Enter 3rd number : "))
+    average = (a+b+c)/3
+    print(average)
+
+#Function Call
+avg()
